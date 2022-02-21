bac08fb794ddef7edde94e0c37ead97455f85e30
Josette Garcia <josettetgarcia@gmail.com>
2022-02-21 15:05:01 -0600

    commit tree 3da5ee779c5ceaa6358658119157da0fca8803ca

--- index.js
+++ index.js
@@ -1,8 +1,33 @@
+// function hasTargetSum(array, target) {
+// for (let i = 0; i < array.length; i++){
+//   for (let j = i + 1; j < array.length; j++){
+//     if (array[j] === target){
+//       return true
+//     } else {
+//       return false
+//     }
+//   }
+// }
+// }
+
 function hasTargetSum(array, target) {
-  // Write your algorithm here
+  for (let i = 0; i < array.length; i++) {
+    const complement = target - array[i];
+    for (let j = i + 1; j < array.length; j++) {
+      if (array[j] === complement) return true;
+    }
+  }
+  return false;
 }
 
+
 /* 
+
+explain problem:
+Our function needs to compare all numbers in our array to the target number. 
+We need to then add each number to itself and other numbers in the array, 
+if any of them added together equal the target, we return true
+otherwise, return false
   Write the Big O time complexity of your function here
 */
 
@@ -12,7 +37,18 @@ function hasTargetSum(array, target) {
 
 /*
   Add written explanation of your solution here
+
+  hahTargetSum(array, target)
+{ for (every item in array)
+    add it to the other numbers in the array
+    if i + j === target  
+      return true
+    if i + j !== target 
+      return false
+
+}
 */
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
